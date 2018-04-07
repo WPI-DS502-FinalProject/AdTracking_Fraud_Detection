b79a8bf9365cd5cb4950ab4830b60ca428d1da13
Anastasia Leshchyk <anas.leshchyk@gmail.com>
2018-04-07 14:25:13 -0400

    tree with confusion matrix and statistics

---
+++ tree_CM.R
@@ -0,0 +1,51 @@
+library(tree)
+library(caret)
+library (randomForest)
+set.seed(1)
+
+# Read the training data that has all 1s
+
+trainData <- read.csv(file="sub_0.csv", header=TRUE, sep=",")
+
+
+
+# Re-assign the labels originally removed during partitioning
+names(trainData) <- c('ip', 'app', 'device', 'os', 'channel', 'click_time', 'attributed_time', 'is_attributed')
+trainData$attributed_time <- NULL
+trainData$click_time <- NULL
+
+# Set  whether as factors or numerical
+trainData$is_attributed = as.factor(trainData$is_attributed)
+trainData$app = as.numeric(trainData$ap)
+trainData$os = as.numeric(trainData$os)
+trainData$channel = as.numeric(trainData$channel)
+trainData$click_time = as.numeric(trainData$click_time)
+trainData$device = as.numeric(trainData$device)
+
+# Split the data to training and test set
+Idx <- sample(floor(nrow(trainData) * 0.8))
+trainSet <- trainData[Idx,]
+testSet <- trainData[-Idx,]
+
+
+#testSet$is_attributed = as.numeric(testSet$is_attributed)
+testSet$is_attributed = as.factor(testSet$is_attributed)
+
+tree_=tree(is_attributed ~ ., data = trainSet)
+
+summary(tree_)
+par(mfrow = c(1, 1))
+plot(tree_)
+par(ps = 8, cex = 1, cex.main = 1)  #Set the size of the font = 8
+text(tree_,pretty=0)
+pred=predict(tree_, newdata = testSet)
+results = seq(1, dim(pred)[1])
+for (i in 1:dim(pred)[1]) {
+  results[i] = c(0, 1)[which.max(pred[i,])]
+}
+pred = results
+
+mean((pred - testSet$is_attributed)^2)  
+u = union(pred, testSet$is_attributed)
+t = table(factor(pred, u), factor(testSet$is_attributed, u))
+confusionMatrix(t)
