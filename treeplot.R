38a73e7e780c0fac42108f6da5e5233253c50adc
Nikolaos Kalampalikis <nkalampalikis@wpi.edu>
2018-04-13 18:28:36 -0400

    Add files via upload

---
+++ treeplot.R
@@ -0,0 +1,14 @@
+par(xpd = NA)
+plot(NA, NA, type = "n", xlim = c(0, 400), ylim = c(0, 30), xlab = "Channel", ylab = "App")
+
+
+
+lines(x = c(0, 400), y = c(18.5, 18.5))
+lines(x = c(0, 400), y = c(19.5, 19.5))
+lines(x = c(0, 400), y = c(28.5, 28.5))
+lines(x = c(114.5, 114.5), y = c(0, 18.5))
+lines(x = c(112, 112), y = c(0, 18.5))
+lines(x = c(345, 345), y = c(28.5, 30))
+
+
+
